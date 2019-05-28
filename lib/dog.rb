d33637a00d91572ebab3bc499b10a42cfcd7ed92
Jess Rezac <jess.rezac@gmail.com>
2019-05-28 02:48:44 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,20 @@
+class Dog
+  @@all = []
+
+  attr_reader :name
+  
+  def self.clear_all
+    @@all = []
+  end
+
+  def self.all
+    @@all.each do |dog|
+      puts dog.name
+    end
+  end
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+end
